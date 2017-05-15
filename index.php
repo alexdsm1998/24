a6d5a075af311e492f10def21f8946e830c68b97
ALUMNADO\aldema <alexdelsaz@gmail.com>
2017-05-15 09:33:41 +0200

    no la pude subir desde mi casa

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>NBA</title>
+    <style media="screen">
+    </style>
+  </head>
+  <body>
+    <table border="2px">
+
+<tr>
+  <td ><h4><a href="insertarequipo.php"> INSERTAR UN NUEVO EQUIPO</a></h4></td>
+  <td ><h4><a href="insertarjugador.php">INSERTAR UN NUEVO JUGADOR</a></h4></td>
+</tr>
+
+    </table>
+
+
+  </body>
+</html>
